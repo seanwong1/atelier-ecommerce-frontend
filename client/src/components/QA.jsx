bb2e129b1d70a8079a5fb2e2554b19feebb4b766
Alvino Handiman <alvinohandiman004@gmail.com>
2023-03-09 00:13:29 -0500

    Implemented initial get request to server from questions and answers api for specific product, given an id

--- client/src/components/QA.jsx
+++ client/src/components/QA.jsx
@@ -1,7 +1,23 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import QACard from './QACard.jsx';
 
 const QA = ({ id }) => {
+const [questions, setQuestions] = useState([]);
+
+async function fetchQuestions() {
+  try {
+    let result = await axios.get('/questions');
+    console.log(result.data)
+    setQuestions(result.data);
+  } catch(err) {
+    console.log(err);
+  }
+}
+
+useEffect(() => {
+  fetchQuestions();
+}, [])
 
   return (
     <div className='QA'>
--- server/index.js
+++ server/index.js
@@ -17,13 +17,34 @@ app.get('/product', (req, res, next) => {
   }
 
   axios.request(options).then((data) => {
-    console.log(data.data);
+    // console.log(data.data);
     res.send(data.data);
   }).catch((err) => {
     console.log(err);
     res.sendStatus(404);
   })
 });
+
+app.get('/questions', async (req, res) => {
+  let options = {
+    'method': 'get',
+    'url': api.QUESTIONS,
+    'params': {
+      'product_id': 71697
+    },
+    'headers': {
+      'Authorization': api.TOKEN
+    }
+  }
+  // res.send('working')
+  try {
+    let questions = await axios.request(options);
+    console.log(questions.data.results);
+    res.send(questions.data.results)
+  } catch(err) {
+    console.log(err);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Listening on ${port}`)
