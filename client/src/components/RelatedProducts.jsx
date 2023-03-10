f6c2ec26cc57e442539f26676b45d270423f80b8
Sean Wong <sean.uuong@gmail.com>
2023-03-09 19:24:42 -0800

    Add skeleton, display related product IDs

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -1,10 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+const apiURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/'
+const api = require('../../../config.js');
 
 const RelatedProducts = (props) => {
-  console.log(props);
+  const [relatedProducts, setRelatedProducts] = useState([]);
+
+  const getRelatedProducts = () => {
+    let options = {
+      'url': api.URL + '/related',
+      'method': 'get',
+      'headers': {
+        'Authorization': api.TOKEN
+      }
+    }
+
+    axios.request(options).then((data) => {
+      setRelatedProducts(data.data);
+    })
+  };
+
+  useEffect(() => {
+    getRelatedProducts();
+  }, [])
+
   return (
     <div className='related-products'>
-      hello, world
+      {relatedProducts.map((relatedProduct) => {
+        return (
+          <div onClick={() => {console.log(relatedProduct)}}>
+            {relatedProduct}
+          </div>
+        )
+      })}
     </div>
   )
 }
