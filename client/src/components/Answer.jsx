17dbd097e774bb17c51f8741767758dd32c6b073
Alvino Handiman <alvinohandiman004@gmail.com>
2023-03-11 18:15:22 -0500

    Attempting to display QA cards as a list using data from Atelier api - using Answer component now

---
+++ client/src/components/Answer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Answer = ({ id, body, name, date, helpfulness, reported }) => {
+
+  return (
+    <div className='Answer' key={id}>
+      <p>A: {body}</p>
+      <span className='Answer-NameDate'>By {name}, {date}</span>
+      <span className='Answer-helpful'> Helpful? </span>
+      <span> Yes {helpfulness} </span>
+      <span className='Answer-report'> Report/Reported </span>
+  </div>
+  );
+}
+
+export default Answer
