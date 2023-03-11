be4663edf890a3614c54e28bcc21ff6370862284
Alvino Handiman <alvinohandiman004@gmail.com>
2023-03-11 17:43:24 -0500

    Attempting to display QA cards as a list using data from Atelier api

--- client/src/components/QA.jsx
+++ client/src/components/QA.jsx
@@ -3,39 +3,100 @@ import axios from 'axios';
 import QACard from './QACard.jsx';
 
 const QA = ({ id }) => {
-const [questions, setQuestions] = useState([]);
-const sortQuestions = (arrayToSort) => {
-  let copy = [...arrayToSort];
-  copy.sort((a, b) => {
-    return a['question_helpfulness'] - b['question_helpfulness']
-  });
-
-  return copy
-};
-
-const helpfulQuestions = useMemo(() => {
-  return sortQuestions(questions);
-}, [questions]);
-
-async function fetchQuestions() {
-  try {
-    let result = await axios.get('/questions');
-    setQuestions(result.data);
-  } catch(err) {
-    console.log(err);
+  const [questions, setQuestions] = useState([{
+    "question_id": 37,
+    "question_body": "Why is this product cheaper here than other sites?",
+    "question_date": "2018-10-18T00:00:00.000Z",
+    "asker_name": "williamsmith",
+    "question_helpfulness": 4,
+    "reported": false,
+    "answers": {
+      68: {
+        "id": 68,
+        "body": "We are selling it here without any markup from the middleman!",
+        "date": "2018-08-18T00:00:00.000Z",
+        "answerer_name": "Seller",
+        "helpfulness": 4,
+        "photos": []
+        // ...
+      }
+    }
+  },
+  {
+    "question_id": 38,
+    "question_body": "How long does it last?",
+    "question_date": "2019-06-28T00:00:00.000Z",
+    "asker_name": "funnygirl",
+    "question_helpfulness": 2,
+    "reported": false,
+    "answers": {
+      70: {
+        "id": 70,
+        "body": "Some of the seams started splitting the first time I wore it!",
+        "date": "2019-11-28T00:00:00.000Z",
+        "answerer_name": "sillyguy",
+        "helpfulness": 6,
+        "photos": [],
+      },
+      78: {
+        "id": 78,
+        "body": "9 lives",
+        "date": "2019-11-12T00:00:00.000Z",
+        "answerer_name": "iluvdogz",
+        "helpfulness": 31,
+        "photos": [],
+      }
+    }
   }
-};
+]);
 
-useEffect(() => {
-  fetchQuestions();
-}, []);
+  const sortQuestions = (arrayToSort) => {
+    let copy = [...arrayToSort];
+    copy.sort((a, b) => {
+      return a['question_helpfulness'] - b['question_helpfulness']
+    });
 
+    return copy
+  };
+
+  const helpfulQuestions = useMemo(() => {
+    return sortQuestions(questions);
+  }, [questions]);
+
+
+  useEffect((() => {
+    async function fetchQuestions() {
+      try {
+        let result = await axios.get('/questions');
+        setQuestions(result.data);
+      } catch(err) {
+        console.log(err);
+      }
+    };
+
+    fetchQuestions();
+  }), []);
 
 
   return (
     <div className='QA'>
-      Q&A for {JSON.stringify(helpfulQuestions)}
-      <QACard id={id} />
+      {/* Q&A for {JSON.stringify(helpfulQuestions)} */}
+      {/* Button to add question  */}
+      {
+      helpfulQuestions.map((question) =>
+        <QACard
+        key={question.question_id}
+        id={question.question_id}
+        question={question.question_body}
+        // date={question['question_date']}
+        // name={question['asker_name']}
+        helpfulness={question.question_helpfulness}
+        reported={question.reported}
+        answers={question.answers}
+        />
+      )
+        }
+      {/* Button to expand question list */}
     </div>
   );
 }
--- client/src/components/QACard.jsx
+++ client/src/components/QACard.jsx
@@ -1,21 +1,28 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
-const QACard = ({ id }) => {
+const QACard = ({ id, question, helpfulness, reported, answers }) => {
+  const answer_component = useMemo(() =>
+    Object.values(answers).forEach((answer) => {
+      return (
+        <div className='Answer' key={answer.id}>
+          <p>A: {answer.body}</p>
+          <span className='Answer-NameDate'>By {answer.name}, {answer.date}</span>
+          <span className='Answer-helpful'> Helpful? </span>
+          <span> Yes {answer.helpfulness} </span>
+          <span className='Answer-report'> Report/Reported </span>
+        </div>
+      )
+    })
+  , [answers]);
+  console.log(answer_component)
 
   return (
     <div className='QACard'>
+      Yes
       QACard id={id}
-      <div  className='Question'>Q: Was this question helpful?</div>
+      <div  className='Question'>Q: {question}</div>
       <div className='Question-add'>Add answer</div>
-      <div className='Answers'>
-          <p className='Answer'>
-            A: This is the answer to question number 1.
-            If it is helpful, please upvote.
-          </p>
-          {/* Might not use span element */}
-          <span className='Answer-NameDate'>By Wity101, March 6th, 2023</span>
-          <span className='Answer-helpful'>Helpful?</span> <span>Yes 15</span> <span className='Answer-report'>Report</span>
-      </div>
+      {answer_component}
     </div>
   );
 }
