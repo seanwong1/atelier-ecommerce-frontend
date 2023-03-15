4f5113b3edda180c0a34efb55e4ebd43b43914d7
Alvino Handiman <alvinohandiman004@gmail.com>
2023-03-14 20:55:12 -0400

    Created Add a question Modal and Add a question button, without connection to server

--- client/src/components/QA.jsx
+++ client/src/components/QA.jsx
@@ -1,9 +1,16 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import { createPortal } from 'react-dom';
 import axios from 'axios';
 import QACard from './QACard.jsx';
+import AddQuestion from './AddQuestion.jsx';
 
 const QA = ({ id }) => {
   const [questions, setQuestions] = useState([]);
+  const [question, setQuestion] = useState('');
+  const [answer, setAnswer] = useState('');
+  const [nickname, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [showQuestionModal, setShowQuestionModal] = useState(false);
   const [showMore, setShowMore] = useState(false);
 
   const sortQuestions = (arrayToSort) => {
@@ -44,6 +51,46 @@ const QA = ({ id }) => {
     fetchQuestions();
   }), []);
 
+  const onSubmitQuestion = (e) => {
+    e.preventDefault();
+    let body = {
+      'body': question,
+      'name': username,
+      'email': email,
+      'product_id': id
+    }
+  };
+
+  const onSubmitAnswer = (e) => {
+    e.preventDefault();
+    let body = {
+      'body': answer,
+      'name': username,
+      'email': email,
+      'product_id': id
+    }
+  };
+
+  const onChangeQuestion = (e) => {
+    setQuestion(e.target.value);
+  };
+
+  const onChangeAnswer = (e) => {
+    setAnswer(e.target.value);
+  };
+
+  const onChangeUsername = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const onChangeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const toggleShowQuestionModal =  (e) => {
+    setShowQuestionModal(!showQuestionModal);
+  };
+
   const toggleShowMore = (e) => {
     setShowMore(!showMore);
   };
@@ -51,7 +98,6 @@ const QA = ({ id }) => {
 
   return (
     <>
-      {/* Button to add question if */}
       <div className='QA'>
         {/* Q&A for {JSON.stringify(helpfulQuestions)} */}
         {/* {helpfulQuestions} */}
@@ -65,6 +111,21 @@ const QA = ({ id }) => {
         !showMore && helpfulQuestions.length > 2
         && <button className='QA-ShowMore' onClick={toggleShowMore}>More Answered Questions</button>
         }
+        {/* Button to add question if */}
+        <button onClick={toggleShowQuestionModal}>Ask a question</button>
+        {/* AddQuestion Modal here */}
+        {/* Pass product name as proops to AddQuestion */}
+        {/* Double check if Addquestion is a Modal/test for it */}
+        {showQuestionModal && createPortal(
+          <AddQuestion
+            product_id={id}
+            onChangeQuestion={onChangeQuestion}
+            onChangeUsername={onChangeUsername}
+            onChangeEmail={onChangeEmail}
+            onSubmitQuestion={onSubmitQuestion}
+          />
+          ,document.body
+        )}
       </div>
     </>
   );
