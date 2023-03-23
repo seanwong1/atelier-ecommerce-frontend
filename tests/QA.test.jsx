c0989a87e993dbd8a62aba83af93cb13ca5ca6c8
Alvino Handiman <alvinohandiman004@gmail.com>
2023-03-22 23:20:12 -0400

    Added testing file

---
+++ tests/QA.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import QA from '../client/src/components/QA.jsx';
+import QACard from '../client/src/components/QACard.jsx';
+import Answer from '../client/src/components/Answer.jsx';
+
+/**
+ * @jest-environment jsdom
+ */
+
+// test('QA Components', () => {
+//     render(<QA />);
+
+//     expect(screen.getByRole('button', {name: 'Ask a question'})).toBeInTheDocument();
+
+// })
+
+describe('QA', () => {
+  it('renders QA components', () => {
+    render(<QA />);
+
+    expect(screen.getByText('QACard')).toBeInTheDocument();
+  });
+});
