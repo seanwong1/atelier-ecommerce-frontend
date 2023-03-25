40a07da48b12ce1fbd78dd0667f9f7f107cb217f
Alvino Handiman <alvinohandiman004@gmail.com>
2023-03-25 14:20:17 -0400

    Changed classnames within the Modal components

--- client/src/components/AddAnswer.jsx
+++ client/src/components/AddAnswer.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 const AddAnswer = ({ product_name, question_body, onChangeAnswer, onChangeNickname, onChangeEmail,  onSubmitAnswer }) => {
 
   return (
-    <div className='QA-AddAnswer'>
+    <div className='QA-Modal'>
       <h1>Submit your Answer</h1>
       <h2>{product_name}: {question_body}</h2>
-      <form className='QA-AddAnswerModal'>
+      <form className='QA-Modal-Form'>
         <h3>Your Answer*</h3>
         <textarea
-        className='QA-AddAnswer-Text'
+        className='QA-Modal-Text'
         maxLength='1000'
         onChange={onChangeAnswer}
         />
         <h3>What is your nickname*</h3>
         <input
-        className='QA-Answer-Input'
+        className='QA-Modal-Input'
         maxLength='60'
         placeholder='Example: jack543!'
         onChange={onChangeNickname}
@@ -23,7 +23,7 @@ const AddAnswer = ({ product_name, question_body, onChangeAnswer, onChangeNickna
         <span>For privacy reasons, do not use your full name or email address</span>
         <h3>Your Email*</h3>
         <input
-        className='QA-Answer-Input'
+        className='QA-Modal-Input'
         type='email'
         maxLength='60'
         placeholder='Example: jack@email.com'
