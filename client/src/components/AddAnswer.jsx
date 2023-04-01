8a79ef039e840447587c4f45f94414a59a7a939d
Alvino Handiman <alvinohandiman004@gmail.com>
2023-04-01 15:56:32 -0400

    Committing to change branch

--- client/src/components/AddAnswer.jsx
+++ client/src/components/AddAnswer.jsx
@@ -3,19 +3,19 @@ import React from 'react';
 const AddAnswer = ({ product_name, question_body, onChangeAnswer, onChangeNickname, onChangeEmail,  onSubmitAnswer }) => {
 
   return (
-    <div className='QA-AddAnswer'>
+    <div className='QA-Modal'>
       <h1>Submit your Answer</h1>
       <h2>{product_name}: {question_body}</h2>
-      <form className='QA-AddAnswerModal'>
+      <form className='QA-Form'>
         <h3>Your Answer*</h3>
         <textarea
-        className='QA-AddAnswer-Text'
+        className='QA-Textarea'
         maxLength='1000'
         onChange={onChangeAnswer}
         />
         <h3>What is your nickname</h3>
         <input
-        className='QA-Answer-Input'
+        className='QA-Input'
         maxLength='60'
         placeholder='Example: jack543!'
         onChange={onChangeNickname}
@@ -23,7 +23,7 @@ const AddAnswer = ({ product_name, question_body, onChangeAnswer, onChangeNickna
         <span>For privacy reasons, do not use your full name or email address</span>
         <h3>You Email*</h3>
         <input
-        className='QA-Answer-Input'
+        className='QA-Input'
         maxLength='60'
         placeholder='Example: jack@email.com'
         onChange={onChangeEmail}
