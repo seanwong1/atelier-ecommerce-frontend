1a583292b69f05a6d2d361c5c298a127f6ad2f94
Sean Wong <sean.uuong@gmail.com>
2023-04-25 22:20:46 -0700

    Add file loader

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,12 @@ module.exports = {
           loader: "babel-loader",
         },
       },
+      {test: /\.(png|jpe?g|gif)$/i,
+      use:
+        {
+          loader: 'file-loader',
+        },
+      }
     ]
   },
 
