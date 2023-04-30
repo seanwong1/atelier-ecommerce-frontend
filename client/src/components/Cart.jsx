c2e84b37314fa20c20e3946e4cd5522787500556
Sean Wong <sean.uuong@gmail.com>
2023-04-29 18:40:07 -0700

    Fix Qty not showing up properly

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -50,115 +50,33 @@ const Cart = ({ product, outfits, setOutfits, cartSubmit, skus, currSku, size, s
       arr.push(i);
     }
     setDrop(arr);
-  }, [stock])
+  }, [stock]);
 
-  const removeOutfit = (e) => {
-    var array = [...outfits]; // make a separate copy of the array
-    for (var i = 0; i < array.length; i++) {
-      if (array[i].id === product.id) {
-        array.splice(i, 1);
-        setOutfits(array);
-        break;
-      }
-    }
-  };
-
-    // const sizeDrop = (e) => {
-    //     return (
-    //         <div style={{ display: showSize }} className='sizeDrop'>
-    //             {Object.keys(skus).map((key) => {
-    //                 return (
-    //                     <div id={key} key={Math.random() * 10000}>
-    //                         <button id={key} value={skus[key].size} onClick={clickButtons} className='size-item'>{skus[key].size}</button>
-    //                     </div>
-    //                 )
-    //             })}
-    //         </div>
-    //     )
-    // };
-
-    // const sizeDrop = (e) => {
-    //   return (
-    //     <select className='sizeDrop' style={{ display: showSize }} >
-    //       {Object.keys(skus).map((key) => {
-    //         return (
-    //           <div id={key} key={Math.random() * 10000}>
-    //             <option id={key} value={skus[key].size} className='size-item'>{skus[key].size}>{skus[key].size}</option>
-    //           </div>
-    //         )
-    //       })}
-    //     </select>
-    //   )
-    // }
-
-    if (stock === 0 || stock === null || skus[currSku] === undefined) {
-      return (
-        <h3>OUT OF STOCK</h3>
-      )
-    } else {
-      return (
-        <form onSubmit={addToCart}>
-          <select value={showSize} onChange={() => {setShowSize(event.target.value);}}>
-            {Object.keys(skus).map((key) => {
-              return (
-                  <option id={key} value={skus[key].size} className='size-item'>{skus[key].size}</option>
-              )
-            })}
-          </select>
-          <select value={showQty} className='QTY'>
-            {drop.map((num) => {
-              return (
-                <option value={num} className='qty-item'>{num}</option>
-              )
-            })}
-          </select>
-          <input type='submit' value='Add to Cart' />
-        </form>
-            // <div className='CartButtons'>
-            //     {/* <QTY  stock={stock} clickButtons={clickButtons} drop={drop}/> */}
-            //     <div className='QTY'>
-
-            //         <button onClick={clickButtons} className='QTY'>Qty: {qty} </button>
-            //         <div style={{ display: showQty }} className='qtyDrop'>
-            //             {drop.map((num) => {
-            //                 return (
-            //                     <div key={Math.random() * 10000} className='qty-item'>
-            //                         <button value={num} onClick={clickButtons} className='qty-item'>{num}</button>
-            //                     </div>
-            //                 )
-            //             })}
-            //         </div>
-
-            //     </div>
-            //     <div className='sizeSelect'>
-            //         <select className='sizeDrop' style={{ display: showSize }} name='size' id='size'>
-            //             {Object.keys(skus).map((key) => {
-            //                 return (
-            //                 <div id={key} key={Math.random() * 10000}>
-            //                     <option id={key} value={skus[key].size} className='size-item'>{skus[key].size}>{skus[key].size}</option>
-            //                 </div>
-            //                 )
-            //             })}
-            //         </select>
-            //     </div>
-
-            //     <button onClick={addToCart} className='Add-to-Cart'>Add to Cart</button>
-
-            //     <button className='Favorite' onClick={(e) => {
-            //         console.log(outfits)
-            //         if (!outfits.some(outfit => outfit.id === product.id)) {
-            //             setOutfits([...outfits, product]);
-            //             setStar('★');
-            //         } else {
-            //             removeOutfit(e);
-            //             setStar('☆');
-            //         }
-            //     }}>{star}
-            //     </button>
-            // </div>
-      )
-
-    }
+  if (stock === 0 || stock === null || skus[currSku] === undefined) {
+    return (
+      <h3>OUT OF STOCK</h3>
+    )
+  } else {
+    return (
+      <form onSubmit={addToCart}>
+        <select value={showSize} onChange={() => {setShowSize(event.target.value);}}>
+        {Object.keys(skus).map((key) => {
+          return (
+            <option id={key} value={skus[key].size} className='size-item'>{skus[key].size}</option>
+          )
+        })}
+        </select>
+        <select value={showQty} onChange={() => {setShowQty(event.target.value);}} className='QTY'>
+          {drop.map((num) => {
+            return (
+              <option value={num} className='qty-item'>{num}</option>
+            )
+          })}
+        </select>
+        <input type='submit' value='Add to Cart' />
+      </form>
+    )
+  }
 }
 
 export default Cart;
